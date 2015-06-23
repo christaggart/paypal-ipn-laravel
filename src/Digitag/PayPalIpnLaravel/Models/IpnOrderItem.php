0f1076c700fcc0b7a9f04a720e875a1c433889e0
Chris Taggart <chris.taggart@canvaspop.com>
2015-06-22 23:42:38 -0400

    fix some compatibility issues w/paypal ipn listener and l5

--- src/Digitag/PayPalIpnLaravel/Models/IpnOrderItem.php
+++ src/Digitag/PayPalIpnLaravel/Models/IpnOrderItem.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
 class IpnOrderItem extends Model {
-    use SoftDeletingTrait;
+    protected $softDelete = true;
 
     protected $dates = ['deleted_at'];
 
